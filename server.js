8462aaacb248dc80ed9e1a01a2598e11c0f660d1
alijalal-sys <ajmabilal@gmail.com>
2020-03-26 20:48:51 +0100

    setting up cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const userRouter = require('./src/routes/user');
 const productRouter = require('./src/routes/product');
 const adminRouter = require('./src/routes/admin');
 
 app.use(express.json());
+app.use(cors());
 app.use('/users', userRouter)
 app.use('/products', productRouter)
 app.use('/admins', adminRouter)
