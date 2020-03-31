9f7ecb570109a3f5f61ab26ed80f52e4e0d6e281
alijalal-sys <ajmabilal@gmail.com>
2020-03-31 10:21:47 +0200

    heroku

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const productRouter = require("./src/routes/product");
 const adminRouter = require("./src/routes/admin");
 const mongoose = require("mongoose");
 // const port = process.env.PORT || 3000;
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config({
-  path: './config.env'
+  path: "./config.env"
 });
 
 app.use(express.json());
@@ -19,8 +19,18 @@ app.use("/admins", adminRouter);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
+});
+
+app.set("port", process.env.PORT || 5000);
+
+// Start node server
+app.listen(app.get("port"), function() {
+  console.log("Node server is running on port " + app.get("port"));
 });
 
 app.get("*", (req, res) => {
@@ -35,7 +45,9 @@ app.get("*", (req, res) => {
 
 mongoose.Promise = global.Promise;
 // Mongodb connection//
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://alijalal:25102000@cluster0-shszn.mongodb.net/test?retryWrites=true&w=majority",
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://alijalal:25102000@cluster0-shszn.mongodb.net/test?retryWrites=true&w=majority",
   {
     useCreateIndex: true,
     useNewUrlParser: true,
@@ -51,8 +63,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://alijalal:25102000@clu
   }
 );
 
-app.listen((process.env.PORT || 3000), (err, res) => {
-  err
-    ? console.log("Error while connecting to sevrer !", err)
-    : console.log(`Connected to server on http://localhost:3000`);
-});
+// app.listen((process.env.PORT || 3000), (err, res) => {
+//   err
+//     ? console.log("Error while connecting to sevrer !", err)
+//     : console.log(`Connected to server on`);
+// });
