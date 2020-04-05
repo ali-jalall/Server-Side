b00e9d01de9fbb40674141e2c255c4796c1d290c
alijalal-sys <ajmabilal@gmail.com>
2020-04-05 15:35:26 +0200

    config

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ mongoose.Promise = global.Promise;
 // Mongodb connection//
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://alijalal:25102000@cluster0-shszn.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://alijalal:0000@cluster0-cbm2v.mongodb.net/test?retryWrites=true&w=majority",
   {
     useCreateIndex: true,
     useNewUrlParser: true,
