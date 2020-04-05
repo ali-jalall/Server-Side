6204996c43b881edcc73a956e291db7d64ef8a7f
alijalal-sys <ajmabilal@gmail.com>
2020-04-05 16:31:24 +0200

    finishing admin controller

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,7 +1,17 @@
 const Admin = require('../models/Admin');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const config = require('../../config');
+
 
 exports.addAdmin = (req, res) => {
-  const email = req.body.email;
+  let email = req.body.email;
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if ( !email || !password || !username ) {
+    res.json({ errMsg: 'Please Fill all the data!' })
+  }
   Admin.findOne({ email })
   .then(admin => {
     if ( admin ) {
@@ -10,7 +20,10 @@ exports.addAdmin = (req, res) => {
         admin
       });
     } else {
-      return Admin.create(req.body);
+      if ( password.length < 8 ) throw new Error('Password Must be more than 8 letters')
+      if ( username.length <= 6 ) throw new Error('Username Must be more than 6 letters')
+      password = bcrypt.hashSync(password, 8)
+      return Admin.create({ username, email, password })
     }
   })
   .then(newAdmin => {
@@ -24,6 +37,28 @@ exports.addAdmin = (req, res) => {
   })
 }
 
+exports.adminLogin = (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  Admin.findOne({ email })
+    .then(admin => {
+      if ( !admin ) {
+        throw new Error('Please Enter Valid Data!');
+      }
+      let isPasswordValid = bcrypt.compareSync(password, admin.password);
+      if ( !isPasswordValid ) res.json({ auth: false, token: null });
+      let token = jwt.sign({ id: admin.id }, config.secret, { expiresIn: 86400 });  
+      res.json({
+        auth: true,
+        admin: admin.username,
+        token
+      })
+    })
+    .catch(err => {
+      res.json({ errMsg: err.message })
+    })
+}
+
 exports.getAdmins = (req, res) => {
   Admin.find()
     .then(admins => {
@@ -31,6 +66,20 @@ exports.getAdmins = (req, res) => {
         res.json({ admins })
       } else {
         throw new Error('No Admins!')
+      }
+    })
+    .catch(err => {
+      res.json({ errMsg: err.message })
+    })
+}
+
+exports.getAdminbyId = (req, res) => {
+  Admin.findOne({ _id: req.params.id })
+    .then(admin => {
+      if ( admin ) {
+        res.json({ admin })
+      } else {
+        throw new Error('No Admin!')
       }
     })
     .catch(err => {
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -10,4 +10,12 @@ router.route('/add')
   .post(admin_controller.addAdmin)
 
 
+router.route('/login')
+  .post(admin_controller.adminLogin)
+
+
+router.route('/:id')
+  .get(admin_controller.getAdminbyId)
+
+
 module.exports = router;
