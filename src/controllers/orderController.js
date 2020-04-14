a243644fd8b7825622125dd50bf2f7b3f6566014
alijalal-sys <ajmabilal@gmail.com>
2020-04-14 17:52:27 +0200

    findProductsByrOrder & findUserByOrder & removeProductFromOrder methods added!

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,49 +1,120 @@
-const Order = require('../models/Order');
+const Order = require("../models/Order");
+const Product = require("../models/Product");
 
 exports.findAllOrders = (req, res) => {
   Order.find()
-    .then(orders => {
-      if ( orders.length === 0 ) throw new Error('No Orders Yet!')
-      res.json({ orders })
+    .then((orders) => {
+      if (orders.length === 0) throw new Error("No Orders Yet!");
+      res.json({ orders });
     })
-    .catch(err => {
-      res.json({ errMsg: err.message })
-    })
-}
+    .catch((err) => {
+      res.json({ errMsg: err.message });
+    });
+};
 
 exports.findOrderById = (req, res) => {
   Order.findOne({ _id: req.params.id })
-    .then(order => {
-      if ( !order ) throw new Error('No product!')
-      res.json({ order })
+    .then((order) => {
+      if (!order) throw new Error("No product!");
+      res.json({ order });
     })
-    .catch(err => {
-      res.json({ errMsg: err.message })
-    })
-}
+    .catch((err) => {
+      res.json({ errMsg: err.message });
+    });
+};
 
 exports.addOrder = (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   Order.create(req.body)
-    .then(order => {
+    .then((order) => {
       res.status(201).json({
-        msg: 'Order added',
-        order
-      })
+        msg: "Order added",
+        order,
+      });
     })
-    .catch(err => {
-      res.json({ errMsg: err.message })
-    })
-}
+    .catch((err) => {
+      res.json({ errMsg: err.message });
+    });
+};
 
 exports.findOrderByIdAndDelete = (req, res) => {
   Order.findByIdAndDelete(req.params.id)
-    .then(order => {
+    .then((order) => {
       res.status(201).json({
-        deleted: true
-      })
+        deleted: true,
+        order
+      });
     })
-    .catch(err => {
-      res.json({ deleted: false, errMsg: err.message })
+    .catch((err) => {
+      res.json({ deleted: false, errMsg: err.message });
+    });
+};
+
+exports.findProductsByOrder = (req, res) => {
+  Order.findOne({ _id: req.params.id })
+    .populate("products_ids")
+    .exec()
+    .then((result) => {
+      if (!result) {
+        throw new Error("Couldn't Find Products!");
+      } else {
+        res.json({ products: result.products_ids });
+      }
     })
-}
+    .catch((err) => {
+      res.json({ errMsg: err.message });
+    });
+};
+
+exports.findUserByOrder = (req, res) => {
+  Order.findOne({ _id: req.params.id })
+    .populate("user_id")
+    .exec()
+    .then(({ user_id }) => {
+      let { email, city, address, phone_number } = user_id;
+      res.json({
+        email,
+        city,
+        address,
+        phone_number,
+      });
+    })
+    .catch((err) => {
+      res.json({ errMsg: err.message });
+    });
+};
+
+exports.findOrderByIdAndUpdate = (req, res) => {
+  Order.findByIdAndUpdate(req.params.id, req.body)
+    .then((order) => res.json(order))
+    .catch((err) => res.josn(err));
+};
+
+exports.removeProductFromOrder = (req, res) => {
+  let _productPrice = 0;
+  Product.findOne({ _id: req.body.id })
+    .then((product) => {
+      if (!product) throw new Error("Product not Exist!");
+      _productPrice = product.price;
+    })
+    .then(() => {
+      return Order.update(
+        { _id: req.params.id },
+        { $inc: { total_price: -_productPrice } }
+      );
+    })
+    .then(() => {
+      return Order.update(
+        { _id: req.params.id },
+        { $pull: { products_ids: req.body.id } }
+      );
+    })
+    .then(() => {
+      return Order.findOne({ _id: req.params.id })
+    })
+    .then(order => {
+      console.log(order)
+      res.status(201).json({ order });
+    })
+    .catch((err) => res.json(err));
+};
