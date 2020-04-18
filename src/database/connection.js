11f1ce05dd686afc89d9b6a5366e048062413e86
alijalal-sys <ajmabilal@gmail.com>
2020-04-18 15:55:52 +0200

    ...

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connection = mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://alijalal:0000@cluster0-cbm2v.mongodb.net/test",
+    "mongodb+srv://alijalal:0000@cluster0-cbm2v.mongodb.net/test?retryWrites=true&w=majority",
   {
     useCreateIndex: true,
     useNewUrlParser: true,
